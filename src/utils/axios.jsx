97edb85d332d9fd4a34f8214b891046e6b32c37e
Dhruv Suhagiya <dhruv.suhagiya11@gmail.com>
2023-07-10 16:09:13 +0530

    change url

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api'
+  baseURL: 'https://tame-erin-fly-veil.cyclic.app/api'
 });
 
 api.interceptors.request.use((config) => {
